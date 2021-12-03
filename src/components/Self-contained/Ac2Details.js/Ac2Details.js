9baa16b0d3d4448440bd7dff261b14b54584a4c7
razon1494 <razon1494@gmail.com>
2021-12-04 01:46:06 +0600

    design home

--- src/components/Self-contained/Ac2Details.js/Ac2Details.js
+++ src/components/Self-contained/Ac2Details.js/Ac2Details.js
@@ -9,13 +9,14 @@ import Details1 from './Details1';
 import Details2 from './Details2';
 import Details3 from './Details3';
 import Details4 from './Details4';
+import ContactUs from '../../ContactUs/ContactUs';
 // ..
 AOS.init();
 const Ac2Details=() => {
     const params=useParams();
     const {id}=params;
     const product=ac2.find(product => product.acid==id);
-    const {brand, model, type, img1,features}=product;
+    const { brand, model, type, img1,features}=product;
     return (
         <div>
             <h2 className='selfac-title text-center my-4'>{brand} {model} {type} Type AC</h2>
@@ -32,7 +33,7 @@ const Ac2Details=() => {
                             features.map(feature => <li className='feature-list-self'>{feature}</li> )
                         }
                     </ul>
-                   <Link to='/products'><button className='btn btn-danger my-4 px-3'>Buy Now</button></Link>
+                <a href="#contact">  <button className='btn btn-danger my-4 px-3'>Buy Now</button></a>
                 </div>
 
 
@@ -45,7 +46,11 @@ const Ac2Details=() => {
             <div data-aos='fade-down'> <Details4 model={model}></Details4></div>
 
 
-
+            <div id="contact"><ContactUs
+                model={model}
+                type1={type}
+                acid={id}
+            ></ContactUs></div>
 
 
         </div>
